56e105e7e6b384f5532bf036f94240836caf4567
Reza Firdaus <mohammad.rezafirdaus@codapayments.com>
2025-01-15 19:01:02 +0700

    update: add pages, package.json, manifest.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 module.exports = {
-	content: [
-		"./templates/*.{html,hbs}",
-		"./templates/**/*.{html,hbs}",
-		"./assets/**/*.css",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./templates/*.{html,hbs}", "./templates/**/*.{html,hbs}", "./assets/**/*.css"],
+  theme: {
+    extend: {
+      colors: {
+        "fc-dark-green": "#04953F",
+        "fc-green": "#07F468",
+        "fc-dark": "#1A1C19",
+      },
+      animation: {
+        marquee: "marquee 30s linear infinite",
+        marquee2: "marquee2 30s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")({ strategy: "class" }), require("daisyui")],
 };
